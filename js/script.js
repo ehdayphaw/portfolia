58189297f9577f6c3cfea40fba8674f2f6f01263
ehdayphaw <nawehdayphaw.2020@gmail.com>
2022-09-19 15:57:09 +0630

    portfolia

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const navbar = document.querySelector('.navbar');
+const up = document.querySelector('#up');
+const toggle = document.querySelector('.toggle');
+const navbarmenu = document.querySelector('.navbar-menu');
+const icon = document.querySelector('.toggle i');
+
+window.onscroll = () => {
+    if(window.scrollY >= 450) {
+        // navbar.classList.add('fixed');
+        // navbar.classList.add('animate__fadeInDown');
+        navbar.classList.add('fixed', 'animate__fadeInDown');
+    }else {
+        navbar.classList.remove('fixed');
+        navbar.classList.remove('animate__fadeInDown');
+    }
+
+    if(window.scrollY >= 800) {
+        up.style.display = 'block'; 
+    }else {
+        up.style.display = 'none';
+    }
+}
+
+up.onclick = () => {
+    window.scrollTo(0,0);
+
+    // window.scrollTo({
+    //     top:0,
+    //     behavior:'smooth',
+    // })
+}
+
+toggle.onclick = () => {
+    navbarmenu.classList.toggle('active');
+    icon.classList.toggle('fa-xmark');
+}
+
+
